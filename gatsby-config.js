2f88575731c9dee9531b3171e93628cf55a8013a
Khris Marcial <khristianmarcial@gmail.com>
2020-04-29 11:29:58 -0400

    adds meta description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'KM',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A blog about my journey, services, and future. I talk about programming, digital marketing, and business',
   },
   plugins: [
     {
